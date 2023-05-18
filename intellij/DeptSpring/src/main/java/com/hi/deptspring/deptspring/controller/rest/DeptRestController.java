e728d06971dfa810e695bb2ff7262f61be6c1161
joungjaehyun <joungjaehyun1008@gmail.com>
2023-05-18 11:11:42 +0900

    start

---
+++ intellij/DeptSpring/src/main/java/com/hi/deptspring/deptspring/controller/rest/DeptRestController.java
@@ -0,0 +1,16 @@
+package com.hi.deptspring.deptspring.controller.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/vi/depts")
+public class DeptRestController {
+
+
+    // get : 전체 리스트
+    // get /{no} : 부서 하나의 정보
+    // post : JSON 형식의 데이터를 받아서 데이터베이스에 저장
+    //
+
+}
